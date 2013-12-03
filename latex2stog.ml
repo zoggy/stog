b5d04dfc0b74ee5777eef480116e7bdb2e6c20bc
Zoggy <zoggy@bat8.org>
2013-12-03 16:42:49 +0100

    begin parsing tex file blocks

--- latex2stog.ml
+++ latex2stog.ml
@@ -35,14 +35,24 @@ type tree =
 and block = {
     tag : string ;
     title : string ;
-    id : string ;
+    id : string option ;
     subs : tree list ;
   }
+
+let rec string_of_tree = function
+  Source s -> "<source>"^s^"</source>"
+| Block b ->
+  "<"^b.tag^" title="^b.title^" id="^(match b.id with None -> "_" | Some s -> s)^">\n"^
+  (string_of_tree_list b.subs)^"\n</"^b.tag^">\n"
+
+and string_of_tree_list l =
+  String.concat "\n" (List.map string_of_tree l)
 
 type tex_file = {
     preambule : string ;
     body : tree list ;
   }
+
 
 let begin_document = "\\begin{document}";;
 let re_begin_document = Str.regexp_string begin_document;;
@@ -59,23 +69,6 @@ let remove_comments s =
   let s = Str.global_substitute re f s in
   (*prerr_endline ("no-comments = "^s);*)
   s
-;;
-
-let rec resolve_includes com tex_file s =
-  let dir = Filename.dirname tex_file in
-  let re_include = Str.regexp ("\\"^com^"{\\([^}]+\\)}") in
-  let f s =
-    let filename = Str.matched_group 1 s in
-    let filename =
-      if Filename.is_relative filename then
-        Filename.concat dir filename
-      else
-        filename
-    in
-    let s = Stog_misc.string_of_file filename in
-    resolve_includes com filename s
-  in
-  Str.global_substitute re_include f s
 ;;
 
 
@@ -100,6 +93,80 @@ let str_cut s_re re ?(start=0) ?(fail=true) s =
         (s, "", "")
 ;;
 
+let mk_blocks com source =
+  prerr_endline ("mk_block for "^com);
+  let re_com = Str.regexp ("\\\\"^com^"{\\([^}]+\\)}") in
+  let len = String.length source in
+  let rec iter acc ?title ?id start =
+    let p_opt =
+      try Some (Str.search_forward re_com source start)
+      with Not_found -> None
+    in
+    match p_opt with
+      None ->
+        prerr_endline (com^" not found");
+        begin
+          match title with
+           None -> (Source (String.sub source start (len - start))) :: acc
+         | Some t ->
+            let body = Source (String.sub source start (len - start)) in
+            let b = { tag = com ; title = t ; id = id ; subs = [ body ] } in
+            (Block b) :: acc
+        end
+    | Some stop ->
+        prerr_endline (com^" found");
+        let acc =
+          begin
+            match title with
+             None -> (Source (String.sub source start (stop - start))) :: acc
+           | Some t ->
+              let body = Source (String.sub source start (stop - start)) in
+              let b = { tag = com ; title = t ; id = id ; subs = [ body ] } in
+              (Block b) :: acc
+          end
+        in
+        let title = Str.matched_group 1 source in
+        let id = None in
+        iter acc ~title ?id (stop+(String.length (Str.matched_string source)))
+  in
+  List.rev (iter [] 0)
+;;
+
+let cut_sectionning =
+  let rec cut body = function
+    [] -> body
+  | com :: q ->
+    let body = List.fold_left (cut_sect com) [] body in
+    cut body q
+
+  and cut_sect com acc = function
+    Block b ->
+      acc @ [Block { b with subs = List.fold_left (cut_sect com) [] b.subs }]
+  | Source s ->
+      acc @ (mk_blocks com s)
+  in
+  fun sections tex ->
+    { tex with body = cut tex.body sections }
+;;
+
+let rec resolve_includes com tex_file s =
+  let dir = Filename.dirname tex_file in
+  let re_include = Str.regexp ("\\"^com^"{\\([^}]+\\)}") in
+  let f s =
+    let filename = Str.matched_group 1 s in
+    let filename =
+      if Filename.is_relative filename then
+        Filename.concat dir filename
+      else
+        filename
+    in
+    let s = Stog_misc.string_of_file filename in
+    resolve_includes com filename s
+  in
+  Str.global_substitute re_include f s
+;;
+
+
 let parse sectionning tex_file =
   let source = Stog_misc.string_of_file tex_file in
   let source = remove_comments source in
@@ -117,7 +184,9 @@ let parse sectionning tex_file =
     in
     (preambule, body)
   in
-  { preambule ; body = [ Source body ] }
+  let tex = { preambule ; body = [ Source body ] } in
+  let tex = cut_sectionning sectionning tex in
+  tex
 ;;
 
 
@@ -148,7 +217,7 @@ let main () =
         let tex = parse !sectionning tex_file in
         match tex.body with
           [ Source s ] -> print_endline s
-        | _ -> assert false
+        | l -> print_endline (string_of_tree_list l)
   with
     Failure msg -> prerr_endline msg ; exit 1
 ;;
