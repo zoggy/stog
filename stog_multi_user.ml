9cf86a621fec4d0aebb90bcd4e348b9df7d230ed
Zoggy <zoggy@bat8.org>
2015-01-28 16:21:13 +0100

    multiserver: display list of sessions

--- stog_multi_user.ml
+++ stog_multi_user.ml
@@ -29,6 +29,7 @@
 
 (** *)
 
+open Stog_types
 open Stog_multi_config
 open Stog_multi_session
 open Stog_multi_gs
@@ -44,6 +45,40 @@ let create_session cfg sessions account =
   let session = Stog_multi_session.create cfg account in
   Stog_multi_gs.add_session session sessions ;
   session
+
+let string_of_date = Netdate.mk_mail_date  
+  
+let session_list cfg gs user =
+  let sessions =
+    Str_map.fold
+      (fun session_id session acc ->
+        if session.session_stored.session_author = user.login then
+          session :: acc
+         else
+           acc
+      )
+      !(gs.sessions)
+      []
+  in
+  let td x = Xtmpl.E(("","td"), Xtmpl.atts_empty, x) in
+  let tds_of_session s = 
+    let preview_url = Stog_types.string_of_url s.session_stog.stog_preview_url in
+    let editor_url = Stog_types.string_of_url s.session_editor.editor_url in
+    let st = s.session_stored in
+    [
+      td [Xtmpl.D (string_of_date st.session_create_date) ] ;
+      td [Xtmpl.D st.session_orig_branch ] ;
+      td [Xtmpl.D st.session_branch ] ;
+      td [Xtmpl.E (("","a"), Xtmpl.atts_one ("","href") [Xtmpl.D preview_url], [Xtmpl.D "preview"])] ; 
+      td [Xtmpl.E (("","a"), Xtmpl.atts_one ("","href") [Xtmpl.D editor_url], [Xtmpl.D "editor"])] ; 
+    ]      
+  in
+  let trs = List.map
+    (fun s -> Xtmpl.E (("","tr"), Xtmpl.atts_empty, tds_of_session s))
+    sessions
+  in
+  [ Xtmpl.E (("","table"), Xtmpl.atts_empty, trs) ]
+   
 
 let handle_sessions_post cfg gs user req body =
   let session = create_session cfg gs.sessions user in
@@ -62,5 +97,7 @@ let handle_sessions_post cfg gs user req body =
   let title = Printf.sprintf "Session %s created" session.session_id in
   Lwt.return (Stog_multi_page.page cfg (Some user) ~title contents)
 
-let handle_sessions_get cfg gs user req body = Lwt.return []
-
+let handle_sessions_get cfg gs user req body =
+  let contents = session_list cfg gs user in
+  let title = "Your sessions" in
+  Lwt.return (Stog_multi_page.page cfg (Some user) ~title contents)
